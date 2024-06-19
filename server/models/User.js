4b76d3221515786865fb7c1cdfa97ab0ebe98a46
Jeet-Chugh <sunjeetchugh@gmail.com>
2024-06-18 19:33:20 -0500

    rename index.js, install react-chessboard, remove redundent dependecies, create User db model

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    created: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
