6d0af1bca80ae3e063ec7c5391d2d427d2ecc9e0
Jeet-Chugh <sunjeetchugh@gmail.com>
2024-06-27 19:46:41 -0500

    created basic frontend pages

---
+++ client/src/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Game from './pages/Game';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/game" component={Game} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
