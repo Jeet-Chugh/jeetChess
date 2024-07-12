10a24a9173f6aa903fea6d3aa5ebdf5e6d57c5d9
Jeet-Chugh <sunjeetchugh@gmail.com>
2024-07-12 18:03:36 -0500

    updated navbar and created logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,180 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+  const [isPlayDropdownOpen, setIsPlayDropdownOpen] = useState(false);
+  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
+
+  const togglePlayDropdown = () => {
+    setIsUserDropdownOpen(false);
+    setIsPlayDropdownOpen(!isPlayDropdownOpen);
+  };
+
+  const toggleUserDropdown = () => {
+    setIsPlayDropdownOpen(false);
+    setIsUserDropdownOpen(!isUserDropdownOpen);
+  };
+
   return (
-    <nav className="bg-gray-800 p-4">
-      <ul className="flex space-x-4">
-        <li><Link to="/" className="text-white">Home</Link></li>
-        <li><Link to="/login" className="text-white">Login</Link></li>
-        <li><Link to="/register" className="text-white">Register</Link></li>
-        <li><Link to="/dashboard" className="text-white">Dashboard</Link></li>
-      </ul>
+    <nav className="bg-gray-900 border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="/logo.png" className="h-8" alt="Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+              JeetChess
+            </span>
+          </Link>
+          <ul className="flex space-x-8 rtl:space-x-reverse">
+            <li>
+              <Link
+                to="/"
+                className="block text-xl ml-3 py-2 px-3 text-white rounded md:text-green-500 md:p-0 hover:text-green-600"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="relative">
+              <button
+                onClick={togglePlayDropdown}
+                className="flex items-center justify-between w-full py-2 px-3 text-xl rounded md:text-green-500 md:p-0 hover:text-green-600"
+              >
+                Play
+                <svg
+                  className="w-2.5 h-2.5 ms-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+              {isPlayDropdownOpen && (
+                <div className="absolute z-10 font-normal bg-gray-900 divide-y divide-gray-100 rounded-lg shadow w-44">
+                  <ul className="py-2 text-sm text-white">
+                    <li>
+                      <Link
+                        to="/dashboard"
+                        className="block px-4 py-2 hover:bg-green-600"
+                      >
+                        Create Game
+                      </Link>                    
+                    </li>
+                    <li>
+                      <Link
+                        to="#"
+                        className="block px-4 py-2  hover:bg-green-600"
+                      >
+                        Join Game
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="#"
+                        className="block px-4 py-2  hover:bg-green-600"
+                      >
+                        My Games
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </li>
+          </ul>
+        </div>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-multi-level">
+          {user ? (
+            <div className="relative">
+              <button
+                onClick={toggleUserDropdown}
+                className="flex items-center"
+              >
+                <FaUserCircle className="w-8 h-8" />
+                <span className="ml-2">{user.substring(0,5)}</span>
+                <svg
+                  className="w-2.5 h-2.5 ms-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+              {isUserDropdownOpen && (
+                <div className="absolute right-0 z-10 font-normal bg-gray-900 divide-y divide-gray-100 rounded-lg shadow w-44">
+                  <ul className="py-2 text-sm text-white">
+                    <li>
+                      <Link
+                        to="/profile"
+                        className="block px-4 py-2 hover:bg-green-600"
+                      >
+                        Profile
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/settings"
+                        className="block px-4 py-2 hover:bg-green-600"
+                      >
+                        Settings
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/help"
+                        className="block px-4 py-2 hover:bg-green-600"
+                      >
+                        Help
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/sign-out"
+                        className="block px-4 py-2 hover:bg-green-600"
+                      >
+                        Sign out
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="flex space-x-4">
+              <Link
+                to="/login"
+                className="py-2 px-4 bg-green-500 text-white rounded hover:bg-green-600"
+              >
+                Login
+              </Link>
+              <Link
+                to="/create-account"
+                className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
+              >
+                Create Account
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
